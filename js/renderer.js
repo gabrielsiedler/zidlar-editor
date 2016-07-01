d27c39bf2066fa56bbb49b67dce5c0b63ca8d9bb
Gabriel Siedler <gabrielsiedler@gmail.com>
2016-07-01 01:33:07 -0300

    Adding loading window

--- js/renderer.js
+++ js/renderer.js
@@ -1,3 +1,28 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const {remote} = require('electron');
+const {dialog} = remote;
+
+window.$ = window.jQuery = require('jquery');
+let modal;
+
+$('.code').on('click', () => $('.code input').focus());
+
+function calcCodeWidth () {
+    var codeWidth = $(window).width() - ($('.menu').width() + $('.submenu').width());
+    $('.main').width(codeWidth);
+}
+
+calcCodeWidth();
+$(window).on('resize', () => { calcCodeWidth(); });
+
+$('.code input').on('click', () => {
+    test();
+});
+
+window.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+  createModal();
+}, false);
+
+function createModal () {
+  modal = dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']})
+}
--- js/script.js
+++
@@ -1,9 +0,0 @@
-$('.code').on('click', () => $('.code input').focus());
-
-function calcCodeWidth () {
-    var codeWidth = $(window).width() - ($('.menu').width() + $('.submenu').width());
-    $('.main').width(codeWidth);
-}
-
-calcCodeWidth();
-$(window).on('resize', () => { calcCodeWidth(); });
